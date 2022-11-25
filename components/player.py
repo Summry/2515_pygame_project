6779e06cdda2b95928525edaf7b90625b57baa67
Nazira Fakhrurradi <nazira.fakhrurradi7@gmail.com>
2022-11-24 20:01:14 -0800

    player can now shoot bullets and will be restricted within the window display

--- components/player.py
+++ components/player.py
@@ -1,4 +1,5 @@
 import pygame
+from .bullet import Bullet
 import os
 
 class Player(pygame.sprite.Sprite):
@@ -25,23 +26,53 @@ class Player(pygame.sprite.Sprite):
 
             self.sprite_animation.append(temp_list)
 
-        self.image = self.sprite_animation[self.action][self.animation_index]
         self.speed = speed
+        self.shoot_cd = 0
         self.jump = False
         self.in_the_air = True
         self.y_velocity = 0
         self.direction = 1
         self.flip_image = False
+        
+        self.image = self.sprite_animation[self.action][self.animation_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+        # Create sprite group for bullets
+        self.bullet_group = pygame.sprite.Group()
+
     def check_limits(self):
         """Make the object stay within the defined limits"""
-        pass
+        if self.rect.left < -5:
+            self.rect.left = -5
+        elif self.rect.right > 970:
+            self.rect.right = 970
+
+    def update(self):
+        self.update_animation()
+        if self.shoot_cd > 0:
+            self.shoot_cd -= 1
 
     def move(self, move_left, move_right, gravity=1):
         x_to_change = 0
         y_to_change = 0
+
+        # Jump
+        if self.jump == True and self.in_the_air == False:
+            self.y_velocity = -17
+            self.jump = False
+            self.in_the_air = True
+        
+        # Gravity
+        self.y_velocity += gravity
+        if self.y_velocity > 10:
+            self.y_velocity = 10
+        y_to_change += self.y_velocity
+
+        # Check for collision from the ground
+        if self.rect.bottom + self.y_velocity > 400:
+            y_to_change = 400 - self.rect.bottom
+            self.in_the_air = False
 
         # Move left
         if move_left:
@@ -55,25 +86,16 @@ class Player(pygame.sprite.Sprite):
             self.flip_image = False
             self.direction = 1
 
-        # Jump
-        if self.jump == True and self.in_the_air == False:
-            self.y_velocity = -17
-            self.jump = False
-            self.in_the_air = True
-
-        # Gravity
-        self.y_velocity += gravity
-        if self.y_velocity > 10:
-            self.y_velocity = 10
-        y_to_change += self.y_velocity
-
-        # Check for collision from the ground
-        if self.rect.bottom + self.y_velocity > 400:
-            y_to_change = 400 - self.rect.bottom
-            self.in_the_air = False
-        
         self.rect.x += x_to_change
         self.rect.y += y_to_change
+
+        self.check_limits()
+
+    def shoot(self):
+        if self.shoot_cd == 0:
+            self.shoot_cd = 60
+            bullet = Bullet(self.rect.centerx + (0.7 * self.rect.width * self.direction), self.rect.centery, self.direction, 0.5)
+            self.bullet_group.add(bullet)
 
     def update_animation(self):
         # Animate
@@ -98,3 +120,5 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self, window):
         window.blit(pygame.transform.flip(self.image, self.flip_image, False), self.rect)
+        self.bullet_group.draw(window)
+        self.bullet_group.update()
