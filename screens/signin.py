62d5b1e2a8047939f551dc9375ec081c04d8671a
Summry <nazira.fakhrurradi7@gmail.com>
2022-11-29 23:39:49 -0800

    Integrated password input onto the signin

--- screens/signin.py
+++ screens/signin.py
@@ -3,44 +3,43 @@ from .base import BaseScreen
 from globalvars import HEIGHT, WIDTH
 
 class SignInScreen(BaseScreen):
-    def __init__(self, window) -> None:
-        super().__init__(window)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.final_score = None
 
         # Display the username message and frame
         self.username = ""
         self.password = ""
-        self.username_check = False
+        self.check_username = False
 
-        # 
+        # Load username message and frame images
         self.username_msg = pygame.image.load("images/username.png").convert_alpha()
-        userframe = pygame.image.load("images/username-frame.png").convert_alpha()
-        self.input_frame = pygame.transform.scale(userframe, (330, 100))
+        input_frame_image = pygame.image.load("images/username-frame.png").convert_alpha()
+        self.input_frame = pygame.transform.scale(input_frame_image, (330, 100))
 
         # Display the username font
         self.username_font = pygame.font.Font("fonts/minecraft.ttf", 30)
-        self.empty_username = True
+        self.retry = False
 
         # Display the password font
         self.password_font = pygame.font.Font("fonts/minecraft.ttf", 30)
-        self.empty_password = True
 
     def draw(self):
         """Method to draw the username frame and message"""
         self.window.blit(self.username_msg, (90, 180))
 
         # Display the username information
-        self.username_text_surface = self.username_font.render(f"{self.username}", False, "Black")
+        username_text_surface = self.username_font.render(f"{self.username}", False, "Black")
         self.window.blit(self.input_frame, (330, 290))
-        self.window.blit(self.username_text_surface, (375, 325))
+        self.window.blit(username_text_surface, (375, 325))
 
         # Display the password information
-        self.password_text_surface = self.password_font.render(f"{self.password}", False, "Black")
-        self.window.blit(self.input_frame, (330, 340))
-        self.window.blit(self.password_text_surface, (375, 375))
+        password_text_surface = self.password_font.render(f"{'*' * len(self.password)}", False, "Black")
+        self.window.blit(self.input_frame, (330, 365))
+        self.window.blit(password_text_surface, (375, 400))
 
     def update(self):
-        if self.empty_username is True or self.empty_password is True:
+        if self.retry == True:
             self.display_retry()
 
     def display_retry(self):
@@ -53,41 +52,39 @@ class SignInScreen(BaseScreen):
     def manage_event(self, event):
         if event.type == pygame.KEYDOWN:
 
-            # Erase the last character of the username string
-            if event.key == pygame.K_BACKSPACE:
-                self.username = self.username[:-1]
+            if self.check_username == False:
 
-            # Confirm that username was typed
-            elif event.key == pygame.K_RETURN and self.empty_username is False:
-                self.username_check = True
+                # Erase the last character of the username string
+                if event.key == pygame.K_BACKSPACE:
+                    self.username = self.username[:-1]
 
-            # Username is empty and return key is pressed
-            elif event.key == pygame.K_RETURN and self.username == "":
-                self.empty_username = True
+                # Confirm that username was typed
+                if event.key == pygame.K_RETURN and len(self.username) > 0:
+                    self.check_username = True
 
-            # Password is empty and return key is pressed
-            elif event.key == pygame.K_RETURN and self.password == "":
-                self.empty_password = True
+                # Username is empty and return key is pressed
+                if event.key == pygame.K_RETURN and len(self.username) == 0:
+                    self.retry = True
 
-            # Password is not empty and return key is pressed
-            elif self.password != "" and event.key == pygame.K_RETURN:
-                self.next_screen = "welcome"
-                self.running = False
-
-            # Make sure the username is not longer than 10 characters
-            elif len(self.username) > 9:
-                self.username = self.username
-
-            # Type username
-            elif self.username_check is False:
-                if event.unicode.isalnum():
-                    if self.empty_username is True:
-                        self.empty_username = False
+                # Type username
+                if event.unicode.isalnum() and len(self.username) < 10:
                     self.username += event.unicode
 
-            # Type password
-            elif self.username_check is True:
-                if event.unicode.isalnum():
-                    if self.empty_password is True:
-                        self.empty_password = False
+            else:
+                
+                # Erase the last character of the password
+                if event.key == pygame.K_BACKSPACE:
+                    self.password = self.password[:-1]
+
+                # Password is empty and return key is pressed
+                if event.key == pygame.K_RETURN and len(self.password) == 0:
+                    self.retry = True
+
+                # Password is not empty and return key is pressed
+                if len(self.password) > 0 and event.key == pygame.K_RETURN:
+                    self.running = False
+                    self.next_screen = "welcome"
+                    
+                # Type password
+                if event.unicode.isalnum() and len(self.password) < 12:
                     self.password += event.unicode
