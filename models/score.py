0b83323c9e22f07f140a7d49458b305e2ecad37b
Summry <nazira.fakhrurradi7@gmail.com>
2022-11-30 00:55:51 -0800

    Added find_user method, delete_score method, and modified add_score method.

--- models/score.py
+++ models/score.py
@@ -35,34 +35,92 @@ class Score:
 
         return scores
 
+    def find_user(self, user, password):
+        """Method to find a user
+
+        Args:
+            user (str): the username to find
+
+        Raises:
+            ValueError: if user is not str
+
+        Returns:
+            boolean: true if user is found
+        """
+        if type(user) is not str:
+            raise ValueError
+        if type(password) is not str:
+            raise ValueError
+
+        if user == "" or password == "":
+            raise ValueError
+
+        for score in self.score_list:
+            if score["username"] == user:
+                return True
+
+        return False
+
+    def delete_score(self, id):
+        """Method to delete a score
+
+        Args:
+            id (str): id of the score
+
+        Raises:
+            ValueError: if id is not str
+
+        Returns:
+            boolean: true if score is deleted
+        """
+        if type(id) is not str:
+            raise ValueError
+
+        for score in self.score_list:
+            if score["id"] == id:
+                self.score_list.remove(score)
+                return True
+
+        return False
+
     def save(self):
         """Method to save and write into the json file
         """
         with open(self.filename, "w") as fp:
             json.dump(self.score_list, fp)
 
-    def add_score(self, username, score, date):
+    def add_score(self, id, username, password, score, date):
         """Method to add a score
 
         Args:
+            id (str): id of the score
             username (str): username of the player
+            password (str): password of the player
             score (int): the score integer
+            date (str): the date of the score
 
         Raises:
-            ValueError: if username is not str or score is not int
-            ValueError: is username is empty str
+            ValueError: if username, password, or date are not str
+            ValueError: if score is not int
+            valueError: if date, username, or password are empty str
 
         Returns:
             boolean: true when score added successfully
         """
 
-        if (type(username) is not str or type(score) is not int or type(date) is not str):
+        if type(id) is not str:
             raise ValueError
-        if username == "":
+        if type(username) is not str or type(score) is not int:
+            raise ValueError
+        if type(date) is not str or type(password) is not str:
+            raise ValueError
+        if username == "" or password == "" or date == "" or id == "":
             raise ValueError
 
         score_dict = {
+            "id": id,
             "username": username,
+            "password": password,
             "score": score,
             "date": date
         }
