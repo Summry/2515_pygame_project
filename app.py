660672cb90608a80a82ddbbe30cabe9900bf237a
Summry <nazira.fakhrurradi7@gmail.com>
2022-11-30 00:53:54 -0800

    Implemented login/logout functionality. Integrated flask sessions. and more...

--- app.py
+++ app.py
@@ -37,16 +37,11 @@ def add_score():
     
     data = request.json
 
-    """
-    data = {
-        "username": string,
-        "score": integer
-    }
-    """
-
     if not data:
         return "Oops, game data not found.", 404
     if "username" not in data.keys() or "score" not in data.keys() or "date" not in data.keys():
+        return "Invalid data.", 400
+    if "id" in data.keys() or "password" in data.keys():
         return "Invalid data.", 400
 
     try:
@@ -56,8 +51,8 @@ def add_score():
     except ValueError:
         return "Invalid data.", 400
 
-@app.route("/user")
-def user():
+@app.route("/user/<username>")
+def user(username):
     """User page route
 
     Methods:
@@ -73,14 +68,14 @@ def user():
     try:
         file = Score("scores.json")
         scores = file.get_scores()
-        return render_template("admin.html", scores=scores), 200
+        return render_template("user.html", scores=scores), 200
     except ValueError:
         return "Invalid data", 400
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """
-    Route to adminstrator login
+    Route to user login
 
     Methods:
         POST
@@ -91,9 +86,11 @@ def login():
     if request.method == "POST":
         try:
             # Get the username and password from the form
-            if request.form["username"] == "admin" and request.form["password"] == "P@ssw0rd":
-                session["username"] = request.form["username"]
-                return redirect(url_for("admin")), 301
+            data = request.form
+            scores = Score("scores.json")
+            if scores.find_user(data["username"], data["password"]):
+                session["username"] = data["username"]
+                return redirect(url_for("user", username=session["username"])), 301
             else:
                 flash("Invalid username or password.")
                 return redirect(url_for("login")), 301
@@ -113,11 +110,38 @@ def logout():
     Returns:
         A document with a status code
     """
+    if "username" not in session:
+        flash("You must be logged in to access this page.")
+        return redirect(url_for("login")), 301
+
     session.pop("username", None)
     return redirect(url_for("home")), 301
 
-# @app.route("/delete", methods=["POST"])
-# def delete_score():
+@app.route("/delete", methods=["POST"])
+def delete_score():
+    """
+    Route to delete a game score
+
+    Methods: 
+        POST
+
+    Returns:
+        A document with a status code
+    """
+    if "username" not in session:
+        flash("You must be logged in to access this page.")
+        return redirect(url_for("login")), 301
+
+    scores = Score("scores.json")
+    
+    data = request.form
+
+    try:
+        scores.delete_score(data["id"])
+        scores.save()
+        return redirect(url_for("user", username=session["username"])), 301
+    except ValueError:
+        return "Invalid data.", 400
 
 if __name__ == "__main__":
     app.run(debug=True)
