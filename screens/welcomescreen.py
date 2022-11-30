f7a7762480c60e3729e0d1ae6437facacd1376d5
Summry <nazira.fakhrurradi7@gmail.com>
2022-11-29 19:04:36 -0800

    Cleaned up code formatting and commenting

--- screens/welcomescreen.py
+++ screens/welcomescreen.py
@@ -12,21 +12,25 @@ class WelcomeScreen(BaseScreen):
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Keep username and final score as None - nothing to do with them here
+        self.username = None
+        self.final_score = None
+
+        # setting up the frames and animation index of the character
         self.frames = []
         self.frame_index = 0
-        self.final_score = None
-        self.username = None
         self.frame_interval = pygame.time.get_ticks()
 
+        # loading the frames of the character
         num_of_files = len(os.listdir("images/player/stand"))
-
         for i in range(0, num_of_files):
             image = pygame.image.load(f"images/player/stand/{i}.png").convert_alpha()
             image = pygame.transform.scale(image, (int(image.get_width() * 0.3), int(image.get_height() * 0.3)))
             self.frames.append(image)
-        
         self.image = self.frames[self.frame_index]
 
+        # creating the start and exit button
         self.start = Button(pygame.image.load("images/red_start.jpg").convert_alpha(), 700, HEIGHT // 1.8, 0.3)
         self.exit = Button(pygame.image.load("images/red_exit.png").convert_alpha(), 700, HEIGHT // 1.3, 0.64)
 
@@ -50,10 +54,13 @@ class WelcomeScreen(BaseScreen):
         """
         animation_interval = 100
         self.image = self.frames[self.frame_index]
+
+        # updating the animation
         if pygame.time.get_ticks() - self.frame_interval > animation_interval:
             self.frame_interval = pygame.time.get_ticks()
             self.frame_index += 1
 
+        # if the animation is finished, start again
         if self.frame_index >= len(self.frames):
             self.frame_index = 0
 
@@ -63,11 +70,15 @@ class WelcomeScreen(BaseScreen):
         Args:
             event (event): an event in pygame
         """
+
+        # if the user clicks on the start button, go to the game screen
         if self.start.draw(self.window):
             button_sound = pygame.mixer.Sound("audio/button-click.mp3")
             button_sound.set_volume(0.3)
             button_sound.play()
             self.next_screen = "game"
             self.running = False
+
+        # if the user clicks on the exit button, exit the game
         if self.exit.draw(self.window):
             self.running = False
